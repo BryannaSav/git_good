a74d742e92b6c57dbf46cda1c175dd3a38af6e94
Ryan Alumbaugh <ralumbaugh@gmail.com>
2021-10-20 21:15:16 -0700

    Time to rebase

--- test.js
+++ test.js
@@ -1,4 +1,1 @@
-console.log("Look at me! I'm dancing, I'm dancing!");
-console.log("Test 1 2 3. Test 1 2 3.");
-console.log("Test 3 2 1. Test 3 2 1");
-console.log("I'm going to make a drawing!");
+console.log("Resistance is futile.");
